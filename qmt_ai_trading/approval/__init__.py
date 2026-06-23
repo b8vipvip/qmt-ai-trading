02dd9a4e88ae4e5d0e84340e985e310d2f2ec303
b8vipvip <917931227@qq.com>
2026-06-23 16:37:02 +0800

    export human approval review builder

--- qmt_ai_trading/approval/__init__.py
+++ qmt_ai_trading/approval/__init__.py
@@ -2,5 +2,14 @@
 
 from .models import ApprovalAction, ApprovalCheckResult, ApprovalDecision, ApprovalRequest, ApprovalStatus
 from .store import ApprovalStore
+from .report import build_human_approval_review
 
-__all__ = ["ApprovalAction", "ApprovalCheckResult", "ApprovalDecision", "ApprovalRequest", "ApprovalStatus", "ApprovalStore"]
+__all__ = [
+    "ApprovalAction",
+    "ApprovalCheckResult",
+    "ApprovalDecision",
+    "ApprovalRequest",
+    "ApprovalStatus",
+    "ApprovalStore",
+    "build_human_approval_review",
+]
