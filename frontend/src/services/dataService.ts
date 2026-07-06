1f8e3bbd384dcb6f902b49d3482e7e72fb7d1093
b8vipvip <917931227@qq.com>
2026-07-06 16:19:31 +0800

    feat: align data service with backend data artifacts

--- frontend/src/services/dataService.ts
+++ frontend/src/services/dataService.ts
@@ -1,14 +1,15 @@
 import { dataQuality, dataSources, dataTasks } from '../mock/mockData';
 import { apiOrMock } from './apiClient';
+import { mapDataQualityRows, mapDataSources, mapDataTasks } from './mappers';
 
 export function getDataSources() {
-  return apiOrMock('/data/sources', dataSources);
+  return apiOrMock('/data/sources', dataSources, (body) => mapDataSources(body.data, dataSources));
 }
 
 export function getDataQualityRows() {
-  return apiOrMock('/data/quality', dataQuality);
+  return apiOrMock('/data/quality', dataQuality, (body) => mapDataQualityRows(body.data, dataQuality));
 }
 
 export function getDataTasks() {
-  return apiOrMock('/data/tasks', dataTasks);
+  return apiOrMock('/data/tasks', dataTasks, (body) => mapDataTasks(body.data, dataTasks));
 }
